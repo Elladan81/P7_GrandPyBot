87dc684d43e4c966e114fde7df8b44c1bffa0135
Micka <35881360+Elladan81@users.noreply.github.com>
2020-01-29 20:56:46 +0100

    create a little template with a simple form for test it works !

--- app_pybot/views.py
+++ app_pybot/views.py
@@ -5,7 +5,7 @@
 Author : Mickael Lalevée
 version 0.1
 """
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -13,18 +13,17 @@ app.config.from_object('config')
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    response = ""
 
-    response = request.form['text']
+    if "submit" in request.form:
+        response = request.form['text']
 
-    return """ 
-    {name}<br/>
-    basic Flask Application
-    <form method = "post" >
-    <input type = "text" name = "text" maxlength = 25>
-    <input type = "submit" name = "submit" value = "Send">
-    </form>
-    Posted data: {response} """.format(name=app.config['APP']['NAME'],
-                                       response=response)
+    return render_template(
+        "index.html",
+        name=app.config['APP']['NAME'],
+        url=app.config['APP']['SRC'],
+        response=response,
+    )
 
 
 if __name__ == "__main__":
