46297c31411099818c01c272ed55ce02f87fa334
Micka <35881360+Elladan81@users.noreply.github.com>
2020-01-29 17:36:20 +0100

    create form and test it

--- app_pybot/views.py
+++ app_pybot/views.py
@@ -5,14 +5,24 @@
 Author : Mickael Lalevée
 version 0.1
 """
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return "Hello world !"
+    response = ""
+
+    if "submit" in request.form:
+        response = request.form['text']
+
+    return """
+    <form method = "post" >
+    <input type = "text" name = "text" maxlength = 25>
+    <input type = "submit" name = "submit" value = "Send">
+    </form>
+    Posted data: {} """.format(response)
 
 
 if __name__ == "__main__":
