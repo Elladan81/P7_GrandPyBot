c2cca8bb47f89687bdcb449356ba86c7da5dcf79
Elladan81 <heladan@hotmail.fr>
2020-02-06 14:03:53 +0100

    add random answers debugging

--- app_pybot/static/js/ajax.js
+++ app_pybot/static/js/ajax.js
@@ -69,7 +69,8 @@ function GrandPyBotRandomAnswer() {
     "Qu'est ce que je peux te dire sur cet endroit... Humm... Ha oui !",
     "Je pense avoir trouvé ce que tu cherches...",
     "Je me rappelle d'une petite chose à propos de cet endroit...",
-    ""
+    "Un jour je me suis perdu dans ce coin... Ho, où suis je ?",
+    "C'est assez proche de l'université où je travaille. Tu ne l'as jamais vu ? Normal. Haha",
     ];
     var grandPyBotAnswer = possibleAnswer[Math.floor(Math.random()*possibleAnswer.length)];
     console.log("Le serveur à choisi"+ grandPyBotAnswer ); // for Debug
@@ -81,7 +82,8 @@ function GrandPyBotRandomDontUnderstand() {
     "Je dois avoir la mémoire qui flanche, ça ne me dit rien...",
     "Peux-tu m'en dire plus ?",
     "Je n'ai rien compris... Tape plus fort sur ton clavier !",
-    "Aucune idée... Je devrais reprendre plus de pilules de grenouilles séchées."
+    "Aucune idée... Je devrais reprendre des pilules de grenouilles séchées.",
+    "Oook. Hmmmm ? Ho, pardon. Je te dis que... je n'ai rien compris.",
     ];
     var dontUnderstandAnswer = dontUnderstandRandomAnswer[Math.floor(Math.random()*dontUnderstandRandomAnswer.length)];
     console.log("Le serveur à choisi"+ dontUnderstandAnswer ); // for Debug
@@ -94,7 +96,7 @@ function GrandPyBotRandomContinue () {
     "Je peux te parler de quelque chose d'autre ?",
     "Tu as une autre question ?",
     "Encore autre chose ?",
-    "Oui ? Il va pleuvoir demain...",
+    "Oui ? Il va pleuvoir demain... Autre chose ?",
     ];
     var currentContinueAnswer = possibleContinueAnswer[Math.floor(Math.random()*possibleContinueAnswer.length)];
     console.log("Le serveur à choisi"+ possibleContinueAnswer ); // For debug
